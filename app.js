ca3e0727f11204c49f3e4a4ef33dff029fedd97b
Cai Zhen <zhenchai0000@gmail.com>
2023-04-18 12:35:31 +0800

    update: logging and able to capture the webhook request

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
 // Related Dependencies
 const bodyParser = require('body-parser');
 const ngrok = require('ngrok');
-
-// Logger
-const pretty = require('pino-pretty');
-const pino = require('pino');
-const logger = pino({
-    transport: {
-        target: 'pino-pretty',
-        options: {
-            colorize: true
-        }
-    }
-});
+const moment = require('moment-timezone');
+const fs = require('fs');
+const CircularJSON = require('circular-json');
 
 // Express App
 const express = require('express');
@@ -21,29 +12,49 @@ const app = express();
 // Parse JSON request body
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    const now = new Date();
+    const log = `${now.toISOString()} ${req.method} ${req.path}\n`;
+
+    fs.appendFile('./logs/app.log', log, (err) => {
+        if (err) console.log(err);
+    });
+
+    next();
+});
+
 // Handle incoming POST requests to /webhook
 app.post('/webhook', (req, res) => {
-    logger.error(req,res);
-    // Extract relevant data from the request
-    const intent = req.body.queryResult.intent.displayName;
-    const timezone = req.body.queryResult.parameters.timezone;
+    logger('request  ' + CircularJSON.stringify(req));
+    logger('response  ' + CircularJSON.stringify(res));
 
-    // Perform desired actions based on the intent
-    // if (intent === 'greeting') {
-        // Use the timezone to calculate current time in user's timezone
-        const currentTime = new Date().toLocaleString('en-US', {
-            timeZone: timezone
-        });
+    let responseText = handleTelegramMessage(req, res);
+    logger('responseText  ' + responseText);
+    
+    // need to figure out how to response back to dialogflow intent message based on condition
+    return res.json(responseText);
 
-        // Generate response message with personalized greeting
-        const response = {
-            fulfillmentText: `Hello! It's currently ${currentTime} in your timezone.`,
-        };
 
-        // Send response back to Dialogflow
-        res.json(response);
-    // }
-    // res.json();
+    // // Extract relevant data from the request
+    // const intent = req.body.queryResult.intent.displayName;
+    // const timezone = req.body.queryResult.parameters.timezone;
+
+    // // Perform desired actions based on the intent
+    // // if (intent === 'greeting') {
+    //     // Use the timezone to calculate current time in user's timezone
+    //     const currentTime = new Date().toLocaleString('en-US', {
+    //         timeZone: timezone
+    //     });
+
+    //     // Generate response message with personalized greeting
+    //     const response = {
+    //         fulfillmentText: `Hello! It's currently ${currentTime} in your timezone.`,
+    //     };
+
+    //     // Send response back to Dialogflow
+    //     res.json(response);
+    // // }
+    // // res.json();
 });
 
 // Start the server
@@ -61,3 +72,41 @@ ngrok.connect({
         return new Error('Ngrok Failed');
     }
 });
+
+getDateTime = () => {
+    let dateObj = new Date;
+    let date = dateObj.toISOString().split('T');
+
+    return date[0] + ' ' + date[1];
+}
+
+function logger (text) {
+    let date = getDateTime();
+    let log = `${date} ${text} \n`;
+    fs.appendFile('./logs/app.log', log, (err) => {
+        if (err) console.log(err);
+    });
+}
+
+// handle user timezone
+function handleTelegramMessage (req, res) {
+    const message = req.body.originalDetectIntentRequest.payload.data;
+
+    // Get the user's timezone offset from the message date
+    const date = new Date(message.date * 1000);
+    const timezoneOffset = date.getTimezoneOffset();
+
+    // Get the user's timezone name from the timezone offset
+    const timezoneName = moment.tz.zone(moment.tz.guess()).abbr(timezoneOffset);
+
+    // Build a response based on the user's timezone
+    let responseText;
+    if (timezoneOffset < 0) {
+        responseText = `It's currently ${moment().utcOffset(timezoneOffset).format('h:mm A')} on the previous day in ${timezoneName}.`;
+    } else {
+        responseText = `It's currently ${moment().utcOffset(timezoneOffset).format('h:mm A')} on ${timezoneName}.`;
+    }
+
+    // Send the response back to the user
+    return responseText;
+}
